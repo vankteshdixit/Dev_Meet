3b3e2a79dda66bf6b41023cc5a221540a03c4d92
Vanktesh <dixitvanktesh2003@gmail.com>
2025-07-20 03:09:57 +0530

    added some validator to not pollute our data

--- src/app.js
+++ src/app.js
@@ -61,10 +61,13 @@ app.patch("/user", async(req,res)=>{
     const userId = req.body.userId;
     const data = req.body;
     try {
-        const user = await User.findByIdAndUpdate({_id:userId}, data);
+        const user = await User.findByIdAndUpdate({_id:userId}, data,{
+            runValidators: true
+        });
+        
         res.send("User updated successfully");
     } catch (error) {
-        res.status(400).send("Something went wrong");
+        res.status(400).send("Update failed"+ error.message);
     }
 })
 
--- src/models/user.js
+++ src/models/user.js
@@ -2,23 +2,47 @@ const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
     firstName: {
-        type: String
+        type: String,
+        required: true
     },
     lastName: {
         type: String
     },
     emailId: {
-        type: String
+        type: String,
+        lowercase: true,
+        trim: true,
+        required: true,
+        unique: true
     },
     password: {
-        type: String
+        type: String,
+        required: true
     },
     age: {
-        type: Number
+        type: Number,
     },
     gender: {
-        type: String
+        type: String,
+        validate(value){
+            if(!["male", "female", "others"].includes(value)){
+                throw new Error("Invalid gender");
+            }
+        },
     },
+    photoUrl: {
+        type: String,
+        default: "https://static.vecteezy.com/system/resources/previews/045/944/199/non_2x/male-default-placeholder-avatar-profile-gray-picture-isolated-on-background-man-silhouette-picture-for-user-profile-in-social-media-forum-chat-greyscale-illustration-vector.jpg"
+    },
+    about: {
+        type: String,
+        default: "Default"
+    },
+    skills: {
+        type: [String]
+    }
+},{
+    timestamps: true,
 });
 
 
